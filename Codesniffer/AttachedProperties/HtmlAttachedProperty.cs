5238fb1e7a68ed0759794f3c5081f9667b40e090
René van Erp <reneverp@gmail.com>
2017-09-30 18:36:58 +0200

    Add WPF application + code visualization experiment

---
+++ Codesniffer/AttachedProperties/HtmlAttachedProperty.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CodeSniffer.AttachedProperties
+{
+    public static class HtmlAttachedProperty
+    {
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached(
+            "Html",
+            typeof(string),
+            typeof(HtmlAttachedProperty),
+            new FrameworkPropertyMetadata(OnHtmlChanged));
+
+        [AttachedPropertyBrowsableForType(typeof(WebBrowser))]
+        public static string GetHtml(WebBrowser d)
+        {
+            return (string)d.GetValue(HtmlProperty);
+        }
+
+        public static void SetHtml(WebBrowser d, string value)
+        {
+            d.SetValue(HtmlProperty, value);
+        }
+
+        static void OnHtmlChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            WebBrowser wb = d as WebBrowser;
+            if (wb != null)
+                wb.NavigateToString(e.NewValue as string);
+        }
+    }
+}
---
+++ Codesniffer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using CodeSniffer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeSniffer.ViewModels
+{
+    class MainWindowViewModel
+    {
+        public string Html
+        {
+            get
+            {
+                Parser parser = new Parser();
+
+                Project proj = new Project();
+                parser.Parse(@"D:\svn\ganttproject-ganttproject-2.8.5\ganttproject-ganttproject-2.8.5\ganttproject\src\net\sourceforge\ganttproject\ChartTabContentPanel.java", proj);
+
+                return StringToHtml(proj.CompilationUnits[0].Classes[0].Text);
+            }
+        }
+
+        private string StringToHtml(string input)
+        {
+            string output = "<html>"
+                            + "<head>"
+                                + "<script src=\"https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js\"></script>"
+                            + "</head>"
+                            + "<pre class=\"prettyprint\">";
+
+            output += input.Replace("\n", "<br/>");
+
+            output += "</pre></html>";
+
+
+            return output;
+        }
+    }
+}
