d4c42ef8147d9424c28376ca66ca8a02fd5f7bba
René van Erp <reneverp@gmail.com>
2018-02-16 01:49:04 +0100

    adapt python script for plotting normal distribution

--- PythonScripts/plottest.py
+++ PythonScripts/plottest.py
@@ -1,11 +1,29 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
+from scipy.stats import norm
 from numpy import genfromtxt
 
-my_data = genfromtxt("c:\\temp\\test.csv", delimiter=',', comments='(', skip_header=True)
+my_data = genfromtxt("D:\git\\afstuderen\codesniffer\CodeSniffer\CodeSniffer.BBN\TrainingsData\\ClassTrainingSet_042_16022018.csv", delimiter=',', comments='(', skip_header=True)
 
 plt.figure()
-plt.pie(my_data[:,1], )
 
+data = my_data[:,5].tolist()
+
+data.sort()
+
+print(data)
+
+std = np.std(data) 
+mean = np.mean(data) 
+
+plt.plot(data, norm.pdf(data,mean,std), '-o')
+plt.hist(data,normed=True)
 
 plt.show()
+
+plt.figure()
+plt.boxplot(data)
+plt.show()
+
+
