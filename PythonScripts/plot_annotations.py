86bf8d84a8aa30ef2b2a86cef5cdb4e22c4630f4
van Erp <rene.vanerp@philips.com>
2018-02-22 23:32:17 +0100

    add annotations plot

---
+++ PythonScripts/plot_annotations.py
@@ -0,0 +1,86 @@
+import os
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons
+import numpy as np
+
+from scipy.stats import norm
+from numpy import genfromtxt
+
+def plot_data(filename, columns, classifier):
+    my_data = genfromtxt(filename, delimiter=',', comments='(', skip_header=False, names=True)
+
+    my_data = np.where([classifier] == "True")
+
+
+    for name in columns:
+        fig = plt.figure()
+
+        data = my_data[name].tolist()
+
+        data.sort()
+
+        std = np.std(data) 
+        print("{0}: {1}".format(name, std))
+        mean = np.mean(data)
+
+        outlierBorder = 0
+
+        prevVal = 0
+        for x in data:
+            outlierBorder = x
+            distance = x - prevVal
+            #if we reach a point with distance greater than 10% above standard deviation we reached the outlier threshold 
+            if(x > 7 * std):
+                outlierBorder = prevVal
+                break
+            prevVal = x
+
+        sub = fig.add_subplot(211)
+        normD = norm.pdf(data,mean,std)
+        plt.plot(data, normD, '-o')
+        plt.hist(data,normed=True)
+ #       plt.axvline(outlierBorder, color="red", linestyle="dashed", marker="8")
+ #       plt.text(outlierBorder, sub.get_ylim()[1] * .8, "outliers > {}".format(round(outlierBorder,2)))
+        plt.title(name)
+        
+        sub = fig.add_subplot(212)
+        plt.boxplot(data, vert=False)
+ #       plt.axvline(outlierBorder, color="red", linestyle="dashed", marker="8")
+ #       plt.text(outlierBorder, sub.get_ylim()[1] * .8, "outliers > {}".format(round(outlierBorder,2)))
+ #       plt.text(outlierBorder, -0.1, "test")    
+
+    plt.show()
+
+def plot_method_data():
+    columns = ["LOC", "CYCLO", "ATFD", "FDP", "LAA", "MAXNESTING", "NOAV"]
+    filename = os.path.dirname(os.path.realpath(__file__)) + "\\..\\CodeSniffer.BBN\\TrainingsData\\MethodTrainingSet_2319_17022018.csv"
+    classifier = "Long_Method"
+    plot_data(filename, columns, classifier)
+
+
+def plot_class_data():
+    columns = ["LOC", "TCC", "WMC", "ATFD"]
+    filename = os.path.dirname(os.path.realpath(__file__)) + "\\..\\CodeSniffer.BBN\\TrainingsData\\ClassTrainingSet_2319_17022018.csv"
+    classifier = "Large_Class"
+    plot_data(filename, columns, classifier)
+
+def plot_verification_method_data():
+    columns = ["LOC", "CYCLO", "ATFD", "FDP", "LAA", "MAXNESTING", "NOAV"]
+    filename = os.path.dirname(os.path.realpath(__file__)) + "\\..\\CodeSniffer.BBN\\VerificationData\\MethodTrainingSet_1357_18022018.csv"
+    classifier = "Long_Method"
+    plot_data(filename, columns, classifier)
+
+def plot_verification_class_data():
+    columns = ["LOC", "TCC", "WMC", "ATFD"]
+    filename = os.path.dirname(os.path.realpath(__file__)) + "\\..\\CodeSniffer.BBN\\VerificationData\\ClassTrainingSet_1357_18022018.csv"
+    classifier = "Large_Class"
+    plot_data(filename, columns, classifier)
+
+
+plot_class_data()
+plot_method_data()
+
+plot_verification_class_data()
+plot_verification_method_data()
+ 
+
